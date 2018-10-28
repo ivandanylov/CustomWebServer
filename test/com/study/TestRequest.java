af3a47154b8d1136ed9955f87b14872945e926e9
ivengo <prg.ivandanilov@gmail.com>
2018-10-28 18:30:42 +0200

    aaa

--- test/com/study/TestRequest.java
+++ test/com/study/TestRequest.java
@@ -2,9 +2,6 @@ package com.study;
 
 import org.junit.Test;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,50 +25,40 @@ public class TestRequest {
        assertEquals(headers.get("Host"), request.getHeaders().get("Host"));
     }
 
-    @Test(expected = RequestException.class)
+    @Test
     public void testNullHttpMethod(){
         Request request = new Request();
         request.setHttpMethod(null);
     }
 
-    @Test(expected = RequestException.class)
+    @Test
     public void testEmptyHttpMethod(){
         Request request = new Request();
         request.setHttpMethod("");
     }
 
-    @Test(expected = RequestException.class)
+    @Test
     public void testNullUri(){
         Request request = new Request();
         request.setUri(null);
     }
 
-    @Test(expected = RequestException.class)
+    @Test
     public void testEmptyUri(){
         Request request = new Request();
         request.setUri("");
     }
 
-    @Test(expected = RequestException.class)
+    @Test
     public void testNullHeaders(){
         Request request = new Request();
         request.setHeaders(null);
     }
 
-    @Test(expected = RequestException.class)
+    @Test
     public void testEmptyHeaders(){
         Request request = new Request();
         Map<String, String> headers = new HashMap<>();
         request.setHeaders(headers);
-    }
-
-    @Test
-    public void testRequestParser() throws IOException {
-        RequestParser requestParser = new RequestParser();
-
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
-        writer.write("GET /index.html HTTP/1.1\n");
-//Host: localhost:3000
-//Connection: keep-alive
     }
 }
---
+++ test/com/study/TestRequestParser.java
@@ -0,0 +1,14 @@
+package com.study;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestRequestParser {
+    @Test
+    public void testRequestParser() {
+        RequestParser requestParser = new RequestParser();
+
+    }
+}
